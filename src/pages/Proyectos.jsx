3f6f505668b1dbaabd2830ecc5f5832cc69b1854
Francisco Estepa Guerra <estepaguerrafrancisco@gmail.com>
2024-05-09 16:40:42 +0200

    botones en movil

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -5,12 +5,11 @@ import mandalorian from "../assets/imagenes/mandalorian.png";
 import gamehype from "../assets/imagenes/gamehype.png";
 import ciclocero from "../assets/imagenes/ciclocero.png";
 import github from "../assets/imagenes/github.png";
-import enlace from "../assets/imagenes/clip.png"
+import enlace from "../assets/imagenes/clip.png";
 import Atropos from "atropos/react";
 
 function Proyectos() {
-
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   const navigate = useNavigate();
   const myAtropos = Atropos({
     el: ".my-atropos",
@@ -29,109 +28,108 @@ function Proyectos() {
 
   const handleBack = () => navigate(-1);
 
- 
-
   return (
     <div className="proyectos">
-      <button className='botonpatata' onClick={handleBack}>🔙</button>
+      <button className="botonpatata" onClick={handleBack}>
+        🔙
+      </button>
 
       <div>
-
         <h1>PROYECTOS</h1>
 
-      {data.proyectos.map((eachData, index) => {
-        return (
-          <div key={index} className="atropos">
-            <hr />
-            <h2>{eachData.nombre}</h2>
-            <Atropos className="my-atropos">
-
-
-              {eachData.nombre === "Mandalorian" && (
-                <div className="button-container">
-                  <img
-                    src={mandalorian}
-                    data-atropos-offset="0"
-                    className="proyecto-img"
-                  />
-                   <a href={eachData.github} target="blank">
-                    <button className="boton">
-                      <img src={github} alt="github" width={"15px"} />
-                    </button>
-                  </a>
-                  <a href={eachData.enlace} target="blank">
-                    <button className="boton2">
-                    <img src={enlace} alt="enlace" width={"15px"} />
-                    </button>
-                  </a>
-                  
-                </div>
-              )}
-              {eachData.nombre === "GAMEHYPE" && (
-                <div className="button-container">
-                  <img
-                    src={gamehype}
-                    data-atropos-offset="0"
-                    className="proyecto-img"
-                  />
-                  <a href={eachData.github} target="blank">
-                    <button className="boton">
-                      <img src={github} alt="github" width={"15px"} />
-                    </button>
-                  </a>
-                  <a href={eachData.enlace} target="blank">
-                    <button className="boton2">
-                    <img src={enlace} alt="enlace" width={"15px"} />
-                    </button>
-                  </a>
-                  
-                </div>
-              )}
-              {eachData.nombre === "Ciclocero" && (
-                <div>
-                <div className="button-container">
-                  <img
-                    src={ciclocero}
-                    data-atropos-offset="0"
-                    className="proyecto-img"
-                  />
-                  <a href={eachData.github} target="blank">
-                    <button className="boton">
-                      <img src={github} alt="github" width={"15px"} />
-                    </button>
-                  </a>
-                  <a href={eachData.enlace} target="blank">
-                    <button className="boton2">
-                    <img src={enlace} alt="enlace" width={"15px"} />
-                    </button>
-                  </a>
-                  
+        {data.proyectos.map((eachData, index) => {
+          return (
+            <div key={index} className="atropos">
+              <hr />
+              <h2>{eachData.nombre}</h2>
+              <Atropos className="my-atropos">
+                {eachData.nombre === "GAMEHYPE" && (
+                  <div className="button-container">
+                    <img
+                      src={gamehype}
+                      data-atropos-offset="0"
+                      className="proyecto-img"
+                    />
+                    {/* <a href={eachData.github} target="blank">
+                      <button className="boton">
+                        <img src={github} alt="github" width={"15px"} />
+                      </button>
+                    </a>
+                    <a href={eachData.enlace} target="blank">
+                      <button className="boton2">
+                        <img src={enlace} alt="enlace" width={"15px"} />
+                      </button>
+                    </a> */}
                   </div>
+                )}
+                {eachData.nombre === "Ciclocero" && (
                   <div>
-                    
+                    <div className="button-container">
+                      <img
+                        src={ciclocero}
+                        data-atropos-offset="0"
+                        className="proyecto-img"
+                      />
+                      {/* <a href={eachData.github} target="blank">
+                        <button className="boton">
+                          <img src={github} alt="github" width={"15px"} />
+                        </button>
+                      </a>
+                      <a href={eachData.enlace} target="blank">
+                        <button className="boton2">
+                          <img src={enlace} alt="enlace" width={"15px"} />
+                        </button>
+                      </a> */}
+                    </div>
                   </div>
-                </div>
-              )}
-              
+                )}
+                {eachData.nombre === "Mandalorian" && (
+                  <div className="button-container">
+                    <img
+                      src={mandalorian}
+                      data-atropos-offset="0"
+                      className="proyecto-img"
+                    />
+                    {/* <a href={eachData.github} target="blank">
+                      <button className="boton">
+                        <img src={github} alt="github" width={"15px"} />
+                      </button>
+                    </a>
+                    <a href={eachData.enlace} target="blank">
+                      <button className="boton2">
+                        <img src={enlace} alt="enlace" width={"15px"} />
+                      </button>
+                    </a> */}
+                  </div>
+                )}
+              </Atropos>
 
-              
-            </Atropos>
+              <a href={eachData.github} target="blank">
+                      <button className="boton">
+                        <img src={github} alt="github" width={"15px"} />
+                      </button>
+                    </a>
+                    <a href={eachData.enlace} target="blank">
+                      <button className="boton2">
+                        <img src={enlace} alt="enlace" width={"15px"} />
+                      </button>
+                    </a>
 
-            <div className="descripcion">
-             <p className="descripcion">{eachData.descripcion}</p>
-             {eachData.paquetes.map((eachPaquete, index) => {
-              return (
-                <span key={index} className="paquetes">
-                  <b>
-                     {eachPaquete} · {""}
-                  </b>
-                </span>
-              )
-             })}
-             </div>
-          </div>
-        );
-      })}
+              <div className="descripcion">
+                <p className="descripcion">{eachData.descripcion}</p>
+                {eachData.paquetes.map((eachPaquete, index) => {
+                  return (
+                    <span key={index} className="paquetes">
+                      <b>
+                        {eachPaquete} · {""}
+                      </b>
+                    </span>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
